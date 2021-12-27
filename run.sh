514914e830e8b4db6148ea9ac7f4ffce4d350106
bom2013 <noam21288@gmail.com>
2021-12-27 23:54:25 +0200

    Omit rm messages, some path fixs, change timeout, change -nographic to -curses

--- run.sh
+++ run.sh
@@ -1,13 +1,13 @@
 echo "Delete old boot.bin"
-rm bin/boot.bin
+rm bin/boot.bin > /dev/null 2>&1
 
 echo "Compile os..."
-nasm -f bin src/boot.asm -o bin/boot.bin -isrc/
+nasm -f bin src/boot/boot.asm -o bin/boot.bin -isrc/
 
 if [ $? -eq 0 ]; then
     echo "Compilation succeed!"
     read
-    timeout --foreground 10 qemu-system-x86_64 -nographic -fda bin/boot.bin  
+    timeout --foreground 5 qemu-system-x86_64 -fda bin/boot.bin -curses
 else
     echo "Compilation faild!"
 fi
