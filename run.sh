89599e58f5f7ea45a1372252fc4f3a7cacb4219d
bom2013 <noam21288@gmail.com>
2021-12-27 17:57:11 +0200

    Better error messges display

--- run.sh
+++ run.sh
@@ -2,14 +2,13 @@ echo "Delete old boot.bin"
 rm bin/boot.bin
 
 echo "Compile os..."
-nasm_output=$(nasm -f bin src/boot.asm -o bin/boot.bin -isrc/ 2>&1)
+nasm -f bin src/boot.asm -o bin/boot.bin -isrc/
 
-if [ "${nasm_output}" != '' ]; then
-    echo "Compilation faild!"
-    echo $nasm_output
-else
+if [ $? -eq 0 ]; then
     echo "Compilation succeed!"
     read
     timeout --foreground 10 qemu-system-x86_64 -nographic -fda bin/boot.bin  
+else
+    echo "Compilation faild!"
 fi
 
