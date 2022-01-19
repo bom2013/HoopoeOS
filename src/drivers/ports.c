eb4867a159682c8df3d487dce9b6d63576911e15
bom2013 <noam21288@gmail.com>
2022-01-19 21:38:52 +0200

    Format

--- src/drivers/ports.c
+++ src/drivers/ports.c
@@ -4,20 +4,20 @@
 unsigned char portReadByte(unsigned short port)
 {
     unsigned char res;
-    __asm__ volatile (
+    __asm__ volatile(
         "inb %%dx, %%al"
-        : "=a" (res)    // mov the result(al) to res
-        : "d" (port)    // set the port(dx) to port
+        : "=a"(res) // mov the result(al) to res
+        : "d"(port) // set the port(dx) to port
     );
     return res;
 }
 
 void portWriteByte(unsigned short port, unsigned char byte)
 {
-    __asm__ volatile (
+    __asm__ volatile(
         "outb %%al, %%dx"
-        :                           // no input
-        : "a" (byte), "d" (port)    // byte to write and port
+        :                      // no input
+        : "a"(byte), "d"(port) // byte to write and port
     );
 }
 
@@ -25,19 +25,19 @@ void portWriteByte(unsigned short port, unsigned char byte)
 unsigned short portReadWord(unsigned short port)
 {
     unsigned short res;
-    __asm__ volatile (
+    __asm__ volatile(
         "in %%dx, %%ax"
-        : "=a" (res)    // mov the result(al) to res
-        : "d" (port)    // set the port(dx) to port
+        : "=a"(res) // mov the result(al) to res
+        : "d"(port) // set the port(dx) to port
     );
     return res;
 }
 
 unsigned short portWriteWord(unsigned short port, unsigned short word)
 {
-    __asm__ volatile (
+    __asm__ volatile(
         "out %%ax, %%dx"
-        :                           // no input
-        : "a" (word), "d" (port)    // byte to write and port
+        :                      // no input
+        : "a"(word), "d"(port) // byte to write and port
     );
 }
