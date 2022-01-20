86b2bea6ae1d213a41cebfd8c581e192ee48151c
bom2013 <noam21288@gmail.com>
2022-01-20 18:06:22 +0200

    Rename constant.h to stddef.h Add size_t

--- src/libc/constant.h
+++
@@ -1,7 +0,0 @@
-#ifndef LIBC_CONSTANT_H_
-#define LIBC_CONSTANT_H_
-
-// define null
-#define NULL 0
-
-#endif // LIBC_CONSTANT_H_
---
+++ src/libc/stddef.h
@@ -0,0 +1,12 @@
+// Simple stddef.h implementation
+
+#ifndef LIBC_STDDEF_H_
+#define LIBC_STDDEF_H_
+
+// Types
+typedef long unsigned int size_t;
+
+// Macro constants
+#define NULL 0
+
+#endif // LIBC_STDDEF_H_
