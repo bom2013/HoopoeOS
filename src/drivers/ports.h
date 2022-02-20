2f1fbc80d1754cff069a73b9c24f19a6ea74d01d
bom2013 <>
2022-02-20 13:49:11 +0200

    Add documentation for port.h

--- src/drivers/ports.h
+++ src/drivers/ports.h
@@ -1,13 +1,38 @@
 // Port utils
+
 #ifndef DRIVERS_PORTS_H_
 #define DRIVERS_PORTS_H_
 
-// port byte in\out
+/**
+ * Read byte from port
+ *
+ * @param port The port to read from
+ * @return Byte readed from the port
+ */
 unsigned char portReadByte(unsigned short port);
+
+/**
+ * Read word from port
+ *
+ * @param port The port to read from
+ * @return Word readed from the port
+ */
+unsigned short portReadWord(unsigned short port);
+
+/**
+ * Write byte to port
+ *
+ * @param port The port to write to
+ * @param byte Byte to write
+ */
 void portWriteByte(unsigned short port, unsigned char byte);
 
-// port word int\out
-unsigned short portReadWord(unsigned short port);
+/**
+ * Write word to port
+ *
+ * @param port The port to write to
+ * @param word Word to write
+ */
 unsigned short portWriteWord(unsigned short port, unsigned short word);
 
 #endif // DRIVERS_PORTS_H_
