c7e559ef26005d5e3116492e5a1ac737b0d2e2ba
bom2013 <noam21288@gmail.com>
2022-02-22 14:46:56 +0200

    Move the EOI sending after the call to IRQ handler

--- src/cpu/isr.c
+++ src/cpu/isr.c
@@ -122,13 +122,13 @@ void registerInterruptHandler(uint8_t interruptNumber, isr_t handler)
 
 void IRQHandler(ISRStackRegisters_t regs)
 {
-    // send EOI to the PIC to enable further interrupt send
-    sendEOI(regs.interrupt_number - 32);
-
     // call the specific interrupt handler function
     if (interruptHandlers[regs.interrupt_number] != 0)
     {
         isr_t handler = interruptHandlers[regs.interrupt_number];
         handler(regs);
     }
+    
+    // send EOI to the PIC to enable further interrupt send
+    sendEOI(regs.interrupt_number - 32);
 }
