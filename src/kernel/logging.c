c06a99a9ab71b43ffd46c3f4acedf01e2c04073d
bom2013 <noam21288@gmail.com>
2024-09-30 23:06:42 +0300

    Refactor logging message format

--- src/kernel/logging.c
+++ src/kernel/logging.c
@@ -4,6 +4,7 @@
 #include "libc/stdio.h"
 #include "libc/stdarg.h"
 #include "libc/string.h"
+#include "drivers/timer.h"
 
 #define MAX_LOGGING_MESSAGE_SIZE (256)
 
@@ -18,10 +19,10 @@ static void log(const char *msg)
 
 void log_info(const char *msg, ...)
 {
-    kprint(msg);
     va_list args;
     va_start(args, msg);
-    char logging_message[MAX_LOGGING_MESSAGE_SIZE] = "[INFO] ";
+    char logging_message[MAX_LOGGING_MESSAGE_SIZE];
+    sprintf(logging_message, "[INFO] [Ticks: %d] ", get_current_tick());
     strcat(logging_message, msg);
     strcat(logging_message, "\n");
 
@@ -39,7 +40,7 @@ void log_error(const char *msg, const char *src_file, const char *function, int 
     va_start(args, line_number);
 
     char logging_message[MAX_LOGGING_MESSAGE_SIZE];
-    sprintf(logging_message, "[Error] [%s in %s:%d]  ", src_file, function, line_number);
+    sprintf(logging_message, "[Error] [Ticks: %d | %s in %s:%d]  ", get_current_tick(), src_file, function, line_number);
     strcat(logging_message, msg);
     strcat(logging_message, "\n");
     char formatted_message[MAX_LOGGING_MESSAGE_SIZE];
